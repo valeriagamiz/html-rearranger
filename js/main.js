1ccd383504ddb72788d610370ead784c3636237c
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-03-08 17:47:03 -0500

    js completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-var $heading prepend(<h1> "Bellow ground veggies" </h1>);
+var $h1one = $('<h1>');
+var $ulone = $('<ul>');
+var $h1two = $('<h1>');
+var $ultwo = $('<ul>');
+
+$h1one.html('Below ground veggies');
+$('body').append($h1one);
+$('body').append($ulone);
+$h1two.html('Above ground veggies');
+$('body').append($h1two);
+$('body').append($ultwo);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $ulone.append($(this));
+  } else {
+    $ultwo.append($(this));
+  }
+});
